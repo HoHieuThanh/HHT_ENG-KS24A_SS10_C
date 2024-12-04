225e9ff9f0bc51451760a962226d8b678880981d
Hồ Hiểu Thanh <laohac.ht29@gmail.com>
2024-12-04 11:20:14 +0700

    Add files via upload

--- 10-6.c
+++ 10-6.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 int main(){
 	
-	int arr[7]={4,2,6,9,4,6,6};
-	int i, index[100], input, count=0;
-	int n = sizeof(arr)/sizeof(int);
-	printf("Nhap vao so can tim: ");
-	scanf("%d", &input);
-	for(i=0; i<n; i++){
-		if(arr[i]==input){
-			index[count]=i;
-			count++;
+	int arr[8]={2,4,1,9,3,8,5,7};
+	int n=sizeof(arr)/sizeof(int);
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<n-1-i; j++)
+		if(arr[j]>arr[j+1]){
+			int temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
 		}
 	}
-	if(count != 0){
-		printf("Phan tu %d o vi tri: ", input);
-		for(i=0; i<count; i++){
-			printf("%d ", index[i]);
-		}
-	}else{
-		printf("Khong tim thay phan tu");
+	printf("Mang sau khi sap xep: ");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
 	
+	int item;
+	printf("\nNhap gia tri can tim: ");
+	scanf("%d", &item);	
+	int start=0;
+	int end=n-1;
+	int mid;
+	int flag=-1;
+
+	while(start<=end){
+		mid=(start+end)/2;
+		if(arr[mid]==item){
+			flag++;
+		}
+		if(arr[mid]>item){
+			end= mid-1;
+		}else if(arr[mid]<item){
+			start=mid+1;
+		}else{
+			if(flag != -1){
+				printf("Phan tu %d o vi tri %d", item, flag);
+				break;
+			}else{
+				printf("Khong tim thay phan tu trong mang");
+				break;
+			}
+		}
+	}
 	return 0;
 }
