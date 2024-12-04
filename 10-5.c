13056044b5a07112ddb487d14b62564efc0e1ad2
Hồ Hiểu Thanh <laohac.ht29@gmail.com>
2024-12-04 11:15:34 +0700

    Add files via upload

---
+++ 10-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	
+	int arr[7]={4,2,6,9,4,6,6};
+	int i, index[100], input, count=0;
+	int n = sizeof(arr)/sizeof(int);
+	printf("Nhap vao so can tim: ");
+	scanf("%d", &input);
+	for(i=0; i<n; i++){
+		if(arr[i]==input){
+			index[count]=i;
+			count++;
+		}
+	}
+	if(count > 0){
+		printf("Phan tu %d o vi tri: ", input);
+		for(i=0; i<count; i++){
+			printf("%d ", index[i]);
+		}
+	}
+	
+	return 0;
+}
